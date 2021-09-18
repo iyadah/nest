3b1125ea7c60f8827652236e1ef8340b52f70725
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-09-18 10:33:32 +0300

    create task

---
+++ task-management/src/tasks/tasks.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Task } from './task.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+  @Post()
+  createTask(@Body() body): Task {
+    return this.tasksService.createTask(body.title, body.description);
+  }
+}
