a1e3363428e4ffcdf3a0d7aca15c25031b2aa6cc
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-09-25 13:56:32 +0300

    connect to postgres

--- task-management/src/config/typorm.cofig.ts
+++ task-management/src/config/typorm.cofig.ts
@@ -1,12 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: 'mydb.chd28lu80wma.ca-central-1.rds.amazonaws.com',
-  port: 3306,
-  username: 'admin',
-  password: 'admin12345',
-  database: 'taskmanager',
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
   entities: [__dirname + '/../**/*.entity.ts'],
   synchronize: true,
 };
